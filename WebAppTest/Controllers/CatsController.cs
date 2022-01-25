efc4ec9b7b0c448c97641b2d92fd10aa2337d739
KamenYu <kamen.yurukov@gmail.com>
2022-01-25 12:32:39 +0200

    Asp.Net beginning

---
+++ WebAppTest/Controllers/CatsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class CatsController : Controller
+    {
+       
+        public IActionResult List()  // /cats/list
+        {
+            //return View(); // returns html
+
+            return Redirect("/cats/search");
+        }
+
+        public IActionResult Details() // /cats/details
+        {
+            return View();
+        }
+
+        public IActionResult Search() // /cats/details
+        {
+            return View();
+        }
+    }
+}
